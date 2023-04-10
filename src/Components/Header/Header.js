de3d1bde3837c45dc63aee0575cc52cf3f17dfd5
bossaj0412 <ajeetcea0412@gmail.com>
2023-04-11 04:52:14 +0530

    name

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = ({change}) => {
     <div className='head-container' > 
       <img src='https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png' alt='ajeet' className={`head-image  head-img-${change}` }/>
 
-    <h1 className={`head-text head-text-${change}`}> Name It! </h1>
+    <h1 className={`head-text head-text-${change}`}> Find Name! </h1>
 
     </div>
   )
